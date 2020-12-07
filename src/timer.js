db65936af962d813df40aa63434b6df3b17e41a7
Grab <apple@MacBook-Pro-3.local>
2020-12-07 14:35:07 +0900

    useEffect 학습
    (cherry picked from commit 4af05baa8efdbd394ec09cf6f9a4890107db68eb)

--- src/timer.js
+++ src/timer.js
@@ -3,13 +3,13 @@ import React from "react";
 function TimerComponent() {
   const [time, setTime] = React.useState(0);
   console.log("컴포넌트 업데이트");
-  function updateTime() {
+  React.useEffect(function () {
     setTime(time + 1);
-  }
+  }, []);
   return (
     <div>
       <h3>{time}초</h3>
-      <button onClick={updateTime}>1씩 올려주세요</button>
+      <button>1씩 올려주세요</button>
     </div>
   );
 }
