bd6ce44f8d79e3aeacb30aacd88b6474a57d5ee0
Grab <apple@MacBook-Pro-3.local>
2020-12-07 15:53:33 +0900

    메인페이지 개발-2

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,23 @@
+import React from "react";
 import "./index.css";
+import axios from "axios";
 
 function MainPage() {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(function () {
+    axios
+      .get(
+        "https://341cf269-c712-4751-a587-2c7fd1b972ec.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        const products = result.data.products;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.error("에러 발생 : ", error);
+      });
+  }, []);
+
   return (
     <div>
       <div id="header">
@@ -14,22 +31,26 @@ function MainPage() {
         </div>
         <h1>판매되는 상품들</h1>
         <div id="product-list">
-          <div className="product-card">
-            <div>
-              <img
-                className="product-img"
-                src="/images/products/keyboard1.jpg"
-              />
-            </div>
-            <div className="product-contents">
-              <span className="product-name">키보드</span>
-              <span className="product-price">50000원</span>
-              <div className="product-seller">
-                <img className="product-avatar" src="images/icons/avatar.png" />
-                <span>그랩</span>
+          {products.map(function (product, index) {
+            return (
+              <div className="product-card">
+                <div>
+                  <img className="product-img" src={product.imageUrl} />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-seller">
+                    <img
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
+                    />
+                    <span>{product.seller}</span>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
       <div id="footer"></div>
