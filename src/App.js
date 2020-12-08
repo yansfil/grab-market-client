21e39fe154d1aa86b556592774d6536d245a58d3
Grab <apple@MacBook-Pro-3.local>
2020-12-08 15:05:12 +0900

    antd 기본 적용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import "./App.css";
 import MainPageComponent from "./main";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link, useHistory } from "react-router-dom";
 import UploadPage from "./upload";
 import ProductPage from "./product";
-
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 function App() {
+  const history = useHistory();
   return (
     <div>
       <div id="header">
         <div id="header-area">
-          <img src="/images/icons/logo.png" />
+          <Link to="/">
+            <img src="/images/icons/logo.png" />
+          </Link>
+          <Button
+            size="large"
+            onClick={function () {
+              history.push("/upload");
+            }}
+            icon={<DownloadOutlined />}
+          >
+            상품 업로드
+          </Button>
         </div>
       </div>
       <div id="body">
--- src/main/index.js
+++ src/main/index.js
@@ -29,7 +29,11 @@ function MainPage() {
         {products.map(function (product, index) {
           return (
             <div className="product-card">
-              <Link className="product-link" to={`/products/${product.id}`}>
+              <Link
+                style={{ color: "inherit" }}
+                className="product-link"
+                to={`/products/${product.id}`}
+              >
                 <div>
                   <img className="product-img" src={product.imageUrl} />
                 </div>
