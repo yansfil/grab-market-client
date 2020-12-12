4fde128990377ce529c433cd9a2a5e4915d0b099
Grab <apple@MacBook-Pro-3.local>
2020-12-12 14:42:55 +0900

    결제하기 구현

--- src/main/index.js
+++ src/main/index.js
@@ -52,6 +52,7 @@ function MainPage() {
         {products.map(function (product, index) {
           return (
             <div className="product-card">
+              {product.soldout === 1 && <div className="product-blur" />}
               <Link
                 style={{ color: "inherit" }}
                 className="product-link"
--- src/product/index.js
+++ src/product/index.js
@@ -4,11 +4,13 @@ import { useEffect, useState } from "react";
 import "./index.css";
 import { API_URL } from "../config/constants";
 import dayjs from "dayjs";
+import { Button, message } from "antd";
 
 function ProductPage() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
-  useEffect(function () {
+
+  const getProduct = () => {
     axios
       .get(`${API_URL}/products/${id}`)
       .then(function (result) {
@@ -17,11 +19,27 @@ function ProductPage() {
       .catch(function (error) {
         console.error(error);
       });
+  };
+
+  useEffect(function () {
+    getProduct();
   }, []);
 
   if (product === null) {
     return <h1>상품 정보를 받고 있습니다...</h1>;
   }
+
+  const onClickPurchase = () => {
+    axios
+      .post(`${API_URL}/purchase/${id}`)
+      .then((result) => {
+        message.info("구매가 완료되었습니다");
+        getProduct();
+      })
+      .catch((error) => {
+        message.error(`에러가 발생했습니다. ${error.message}`);
+      });
+  };
 
   return (
     <div>
@@ -38,6 +56,16 @@ function ProductPage() {
         <div id="createdAt">
           {dayjs(product.createdAt).format("YYYY년 MM월 DD일")}
         </div>
+        <Button
+          id="purchase-button"
+          size="large"
+          type="primary"
+          danger
+          onClick={onClickPurchase}
+          disabled={product.soldout === 1}
+        >
+          재빨리 구매하기
+        </Button>
         <pre id="description">{product.description} </pre>
       </div>
     </div>
